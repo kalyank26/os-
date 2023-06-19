e3e42587fc4f409e428d355e85b677d2fa100e61
KALYAN <117442516+kalyank26@users.noreply.github.com>
2023-06-19 12:02:59 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,32 +1,43 @@
 #include<stdio.h>
-#include<stdio.h>
-
+// Function to find the waiting time for all
+// processes
 void findWaitingTime(int processes[], int n,
                           int bt[], int wt[])
 {
+    // waiting time for first process is 0
     wt[0] = 0;
    
+    // calculating waiting time
     for (int  i = 1; i < n ; i++ )
         wt[i] =  bt[i-1] + wt[i-1] ;
 }
    
+// Function to calculate turn around time
 void findTurnAroundTime( int processes[], int n,
                   int bt[], int wt[], int tat[])
 {
+    // calculating turnaround time by adding
+    // bt[i] + wt[i]
     for (int  i = 0; i < n ; i++)
         tat[i] = bt[i] + wt[i];
 }
    
+//Function to calculate average time
 void findavgTime( int processes[], int n, int bt[])
 {
     int wt[n], tat[n], total_wt = 0, total_tat = 0;
-
+   
+    //Function to find waiting time of all processes
     findWaitingTime(processes, n, bt, wt);
-
+   
+    //Function to find turn around time for all processes
     findTurnAroundTime(processes, n, bt, wt, tat);
-
+   
+    //Display processes along with all details
     printf("Processes   Burst time   Waiting time   Turn around time\n");
    
+    // Calculate total waiting time and total turn
+    // around time
     for (int  i=0; i<n; i++)
     {
         total_wt = total_wt + wt[i];
@@ -46,13 +57,20 @@ void findavgTime( int processes[], int n, int bt[])
 // Driver code
 int main()
 {   int s;
-    printf("Enter the number of processes you want to enter :");
+    //process id's
+    printf("Enter the number of processes you want to enter");
     scanf("%d",&s);
     int processes[s];
-   
+    //Burst time of all processes
+    int  burst_time[s];
+    for(int i=0;i<s;i++){
+        processes[i]=i+1;
+        printf("Enter the process %d burst time",i+1);
+        scanf("%d",&burst_time[i]);
+    }
     int n = sizeof processes / sizeof processes[0];
-  
-    int  burst_time[] = {10, 5, 8,9};
+   
+   
    
     findavgTime(processes, n,  burst_time);
     return 0;
